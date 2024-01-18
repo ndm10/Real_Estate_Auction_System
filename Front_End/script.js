c2d38c79d04a2114dd50c477f878a0cf812d39c0
Nguyen Duc Minh <ndminh1010@gmail.com>
2024-01-19 00:35:49 +0700

    upload new homepage

--- Front_End/script.js
+++
@@ -1,9 +0,0 @@
-function triggerModal() {
-  const myModalEl = document.getElementById("productModel");
-  const modal = new mdb.Modal(myModalEl);
-  modal.toggle();
-}
-
-function myFunction() {
-  alert("KAKAKA");
-}
