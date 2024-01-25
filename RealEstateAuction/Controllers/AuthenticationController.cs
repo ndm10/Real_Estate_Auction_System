e451633dc8b69e203f0a8cbb0c29f558668eaffe
Nguyen Duc Minh <ndminh1010@gmail.com>
2024-01-25 18:54:34 +0700

    update login

--- RealEstateAuction/Controllers/AuthenticationController.cs
+++ RealEstateAuction/Controllers/AuthenticationController.cs
@@ -1,10 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAuction.Models;
 
 namespace RealEstateAuction.Controllers
 {
     public class AuthenticationController : Controller
     {
+        RealEstateContext context = new RealEstateContext();
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(FormCollection form)
+        {
+            var email = form["email"];
+            var password = form["pwd"];
+
+            var account = context.Users.SingleOrDefault(a => a.Email.Equals(email) && a.Password.Equals(password));
+            if (account != null)
+            {
+                //save account to session if login success
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Login failed";
+                return View("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Register()
         {
             return View();
         }
